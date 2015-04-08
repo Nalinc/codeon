6643cfe909dcde2eda11a97dee4529a9b70d72ce
Nalin_C <nalin.chhibber@gmail.com>
2015-04-08 18:21:26 +0530

    Create conditionals.py

---
+++ python/syntax/conditionals.py
@@ -0,0 +1,11 @@
+var = 100
+if var == 200:
+   print ("1 - value= "+str(var))
+elif var == 150:
+   print ("2 - value= "+str(var))
+elif var == 100:
+   print ("3 - value= "+str(var))
+else:
+   print ("4 - value= "+str(var))
+
+print("Good bye!")
